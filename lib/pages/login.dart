2a15627c165ef62eb641b8ad9346b7807f5fcd1a
fcrezza <fachrezanang@gmail.com>
2021-07-23 01:14:27 +0700

    make login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,13 +1,141 @@
 import 'package:flutter/material.dart';
 
+import 'package:cuit/widgets/logo.dart';
+import 'package:cuit/widgets/TextInput.dart';
+import 'package:cuit/widgets/PasswordInput.dart';
+
 class Login extends StatefulWidget {
   @override
   _LoginState createState() => _LoginState();
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController _emailController;
+  TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(body: SafeArea(child: LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints viewportConstraints) {
+        return SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: ConstrainedBox(
+                constraints:
+                    BoxConstraints(minHeight: viewportConstraints.maxHeight),
+                child: IntrinsicHeight(
+                    child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                        alignment: Alignment.topLeft,
+                        margin:
+                            EdgeInsets.only(top: 16.0, right: 20.0, left: 20.0),
+                        child: Logo()),
+                    Expanded(
+                        child: Container(
+                      margin:
+                          EdgeInsets.only(top: 40.0, right: 20.0, left: 20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Masuk",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w900,
+                                color:
+                                    Theme.of(context).textTheme.headline1.color,
+                                fontSize: 28.0),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            "Hai! selamat datang kembali.",
+                            style: TextStyle(
+                                color:
+                                    Theme.of(context).textTheme.bodyText1.color,
+                                fontSize: 16.0),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          TextInput(
+                              hintText: "Email", controller: _emailController),
+                          SizedBox(
+                            height: 14.0,
+                          ),
+                          PasswordInput(
+                            hintText: "Password",
+                            controller: _passwordController,
+                          )
+                        ],
+                      ),
+                    )),
+                    Container(
+                        margin: EdgeInsets.only(
+                            right: 20.0, bottom: 30.0, left: 20.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Belum punya akun?',
+                                  style: TextStyle(
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .bodyText1
+                                          .color,
+                                      fontSize: 15.0),
+                                ),
+                                TextButton(
+                                    onPressed: () =>
+                                        Navigator.pushNamed(context, '/signup'),
+                                    child: Text("Daftar",
+                                        style: TextStyle(
+                                            color: Theme.of(context)
+                                                .textTheme
+                                                .headline1
+                                                .color,
+                                            fontWeight: FontWeight.w700,
+                                            fontSize: 15.0))),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Container(
+                                width: MediaQuery.of(context).size.width,
+                                child: ElevatedButton(
+                                    onPressed: () {
+                                      print(_emailController.text);
+                                      print(_passwordController.text);
+                                    },
+                                    child: Text(
+                                      "Masuk",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w700),
+                                    )))
+                          ],
+                        ))
+                  ],
+                ))));
+      },
+    )));
   }
 }
--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'package:cuit/widgets/logo.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class Onboarding extends StatefulWidget {
@@ -7,8 +9,7 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
-  final Widget logo = SvgPicture.asset('assets/images/logo.svg');
-  final List<Map<String, String>> items = [
+  final List<Map<String, String>> featureItems = const [
     {
       "title": "Selamat datang!",
       "description":
@@ -36,112 +37,69 @@ class _OnboardingState extends State<Onboarding> {
   final _controller = PageController();
   int currentPage = 0;
 
-  void handlePageChange(value) {
-    setState(() {
-      currentPage = value;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     bool isLast = currentPage == 3;
 
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-                margin: EdgeInsets.only(top: 16.0, right: 20.0, left: 20.0),
-                height: 40.0,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    logo,
-                    isLast
-                        ? SizedBox()
-                        : TextButton(
-                            style: ButtonStyle(
-                                overlayColor: MaterialStateProperty.all(
-                                    Color(0xFFF0F0F0))),
-                            onPressed: () =>
-                                Navigator.pushNamed(context, '/signup'),
-                            child: Text(
-                              'Lewati',
-                              style: TextStyle(
-                                  color: const Color(0xFF222222),
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 16.0),
-                            )),
-                  ],
-                )),
-            Expanded(
-                child: PageView.builder(
-                    controller: _controller,
-                    itemCount: items.length,
-                    onPageChanged: handlePageChange,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            height: 180.0,
-                            child: SvgPicture.asset(items[index]["image"]),
-                          ),
-                          SizedBox(
-                            height: 24.0,
-                          ),
-                          Text(items[index]["title"],
-                              style: TextStyle(
-                                  color: Theme.of(context)
-                                      .textTheme
-                                      .headline1
-                                      .color,
-                                  fontWeight: FontWeight.w900,
-                                  fontSize: 24.0)),
-                          SizedBox(
-                            height: 10.0,
-                          ),
-                          Text(items[index]["description"],
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  color: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1
-                                      .color,
-                                  fontSize: 16.0)),
-                        ],
-                      );
-                    })),
-            SizedBox(
-                width: 180.0,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: List.generate(
-                    items.length,
-                    (int index) => indicator(index),
-                  ),
-                )),
-            Container(
-                margin: EdgeInsets.symmetric(vertical: 30.0, horizontal: 20.0),
-                width: MediaQuery.of(context).size.width,
-                child: ElevatedButton(
-                    onPressed: isLast
-                        ? () => Navigator.pushNamed(context, '/signup')
-                        : () => _controller.nextPage(
-                            duration: Duration(milliseconds: 200),
-                            curve: Curves.easeIn),
-                    child: Text(
-                      isLast ? "Memulai" : "Selanjutnya",
-                      style: TextStyle(fontWeight: FontWeight.w700),
-                    )))
-          ],
+    return Scaffold(body: SafeArea(child:
+        LayoutBuilder(builder: (BuildContext context, viewportConstraints) {
+      return SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: ConstrainedBox(
+            constraints:
+                BoxConstraints(minHeight: viewportConstraints.maxHeight),
+            child: IntrinsicHeight(
+                child: Text(
+                    'lol')), // This trailing comma makes auto-formatting nicer for build methods.
+          ));
+    })));
+  }
+}
+
+class FeatureItem extends StatelessWidget {
+  final String image;
+  final String title;
+  final String description;
+
+  FeatureItem({this.image, this.description, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 180.0,
+          child: SvgPicture.asset(image),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        SizedBox(
+          height: 24.0,
+        ),
+        Text(title,
+            style: TextStyle(
+                color: Theme.of(context).textTheme.headline1.color,
+                fontWeight: FontWeight.w900,
+                fontSize: 24.0)),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(description,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Theme.of(context).textTheme.bodyText1.color,
+                fontSize: 16.0)),
+      ],
     );
   }
+}
 
-  AnimatedContainer indicator(index) {
+class Indicator extends StatelessWidget {
+  final bool isActive;
+
+  Indicator({this.isActive});
+
+  @override
+  Widget build(BuildContext context) {
     return AnimatedContainer(
       duration: new Duration(milliseconds: 200),
       height: 5.0,
@@ -150,9 +108,7 @@ class _OnboardingState extends State<Onboarding> {
           borderRadius: BorderRadius.all(
             Radius.circular(10),
           ),
-          color: currentPage == index
-              ? const Color(0xFF888888)
-              : const Color(0xFFF0F0F0)),
+          color: isActive ? const Color(0xFF888888) : const Color(0xFFF0F0F0)),
     );
   }
 }
